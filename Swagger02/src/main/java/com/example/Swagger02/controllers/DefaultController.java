0356a5fc29e2b31ad63c72e9e2214de0e28959e3
AlessioP02 <alessio.alemix41@gmail.com>
2023-01-11 17:22:20 +0100

    Initial commit

---
+++ Swagger02/src/main/java/com/example/Swagger02/controllers/DefaultController.java
@@ -0,0 +1,17 @@
+package com.example.Swagger02.controllers;
+
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class DefaultController {
+
+    @GetMapping
+    @ApiOperation(value = "Says Welcome", notes = "Prints the welcome message for the user")
+    public String getWelcome() {
+        return "Welcome";
+    }
+}
